0dfcfbce3a9807431568f88d5709f0f2ebf12516
0xNick404 <ngrimestt@gmail.com>
2026-03-13 22:16:20 -0400

    fix user_code length

--- App/models/user.py
+++ App/models/user.py
@@ -6,7 +6,7 @@ from App.database import db
 class User(db.Model):
     __tablename__ = "users"
     id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
-    user_code =  db.Column(db.String(8), nullable=False, unique=True, index=True)
+    user_code =  db.Column(db.String(10), nullable=False, unique=True, index=True)
     password_hash = db.Column(db.String(256), nullable=False)
     created_at = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
     

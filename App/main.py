8cca94acf9be4756ae9c0a2d581fba6af2774261
0xNick404 <ngrimestt@gmail.com>
2026-03-18 17:16:21 -0400

    remove unused import

--- App/main.py
+++ App/main.py
@@ -7,7 +7,6 @@ from werkzeug.utils import secure_filename
 from werkzeug.datastructures import  FileStorage
 from App.database import init_db
 from App.config import load_config
-from App.models import User
 
 
 from App.controllers import (
